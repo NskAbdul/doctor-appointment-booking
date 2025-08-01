e4d442ed5dbf20e45676a58c46f3d78c1034d7b6
Nsk Abdul <nsk.abdul786@gmail.com>
2025-08-01 23:51:17 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const PORT = process.env.PORT || 3000;
-mongoose.connect("mongodb+srv://nskabdul786:nskabdul@hmsdb.vakmxsn.mongodb.net/?retryWrites=true&w=majority&appName=hmsdb, {
+mongoose.connect("mongodb+srv://NskAbdul:NskAbdul786@cluster0.dsloo1h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
